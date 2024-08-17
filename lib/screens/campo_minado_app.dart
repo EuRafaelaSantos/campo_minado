347eaf48b7799bf8720b7ea28518656f1a49a0ca
Rafaela Santos <rafaelasantos.informatica@gmail.com>
2024-08-16 21:02:45 -0400

    componente ResultadoWidget

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,11 +1,17 @@
+import 'package:campo_minado/components/resultado_widget.dart';
 import 'package:flutter/material.dart';
+
+_reiniciar() {
+  print('reiniciar...');
+}
 
 class CampoMinado extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Campo Minado'),
+        appBar: ResultadoWidget(
+          venceu: true,
+          onReiniciar: _reiniciar,
         ),
         body: Container(
           child: Text('Tabuleiro'),
